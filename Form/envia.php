0f5c7d26d6ace1bd9ac30e363695a02c1aca4c58
GuiDias13 <Karinemendesdesouza2004@gmail.com>
2024-08-26 18:15:37 -0300

    teste

---
+++ Form/envia.php
@@ -0,0 +1,23 @@
+<?php
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $telefone = addcslashes($_POST['telefone']);
+    $mensagem = addcslashes($_POST['mensagem']);
+    
+    $para = "guilhermedia13@hotmail.com";
+
+    $assunto = "Coleta de dados Portfólio";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: gdias@consenti.com.br"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!")
+    }
+    else{
+        echo("Houve um erro ao enviar o e-mail!")
+    }
+    
+?>
